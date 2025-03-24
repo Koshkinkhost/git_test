e885d06864094a195f09d9258e9eeb6fd439a179
Борис <102375597+Koshkinkhost@users.noreply.github.com>
2025-03-24 09:19:23 +0300

    build

--- api_for_kursach/obj/Debug/net8.0/api_for_kursach.AssemblyInfo.cs
+++ api_for_kursach/obj/Debug/net8.0/api_for_kursach.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api_for_kursach")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+898d99281a9f2c4669a885752dae8b64025c1f59")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8b315d6e5b9029c6e38a6f226b4817a519a0ede")]
 [assembly: System.Reflection.AssemblyProductAttribute("api_for_kursach")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api_for_kursach")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
