de3fab0d704547777b5e8c8b7d7b52c5c0a433d7
Борис <102375597+Koshkinkhost@users.noreply.github.com>
2025-03-24 10:49:33 +0300

    jet_3 commit2

--- api_for_kursach/obj/Release/net8.0/api_for_kursach.AssemblyInfo.cs
+++ api_for_kursach/obj/Release/net8.0/api_for_kursach.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api_for_kursach")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7401e971f0087f64cf3ec6020e97652571db4855")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+943e46ed0861630dbd25d84190cd76627c95f2bb")]
 [assembly: System.Reflection.AssemblyProductAttribute("api_for_kursach")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api_for_kursach")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
